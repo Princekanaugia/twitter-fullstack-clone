e30d465d598fcc457b71e65000a9bd2284d27a2f
Prince Kanaugia <princekanaugia@gmail.com>
2025-04-08 16:53:44 +0530

    added post schema and model and post route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js"
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -26,6 +27,7 @@ app.use(cookieParser()) // to parse the cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.get("/", (req,res) => {
     res.send("server is ready")
