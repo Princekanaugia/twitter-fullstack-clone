0bc37b9414068e45862f6b4a79a5aed940a08598
Prince Kanaugia <princekanaugia@gmail.com>
2025-04-10 03:36:04 +0530

    added get liked post route and controller also updated user model with likedposts schema key

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,6 +2,61 @@ import { v2 as cloudinary } from "cloudinary";
 
 import User from "../models/user.model.js";
 import Post from "../models/post.model.js";
+import Notification from "../models/notification.model.js"
+
+export const getAllPosts = async (req, res) => {
+    try {
+        const posts = await Post.find()
+        .sort({ createdAt: -1 })
+        .populate({
+            path: "user",
+            select: "-password",
+        })
+        .populate({
+            path: "comments.user",
+            select: "-password",
+        })
+
+        if (posts.length === 0 ) {
+            return res.status(200).json({ message: "No Post Found" })
+        } 
+
+        res.status(200).json(posts)
+
+    } catch (error) {
+        console.log("Error in getAllPosts Controller: ", error.message)
+        res.status(500).json({error: 'Internal Server Error'})
+    }
+}
+
+export const getLikedPosts = async (req, res) => {
+    const userId = req.params.id
+
+    try {
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" })
+        }
+
+        const likedPosts = await Post.find({ _id: { $in: user.likedPosts }})
+            .sort({ createdAt: -1 })
+            .populate({
+                path: "user",
+                select: "-password",
+            }) 
+            .populate({
+                path: "comments.user",
+                select: "-password",
+            })
+
+        res.status(200).json(likedPosts)
+
+    } catch (error) {
+        console.log("Error in getLikedPosts Controller: ", error.message)
+        res.status(500).json({error: 'Internal Server Error'})
+    }
+}
 
 export const createPost = async (req, res) => {
     try {
@@ -41,7 +96,39 @@ export const createPost = async (req, res) => {
 
 export const likeUnlikePost = async (req, res) => {
     try {
-        
+        const userId = req.user._id;
+        const {id:postId} = req.params;
+
+        const post = await Post.findById(postId)
+
+        if (!post) {
+            return res.status(404).json({ error: "Post not found" })
+        }
+
+        const userLikedPost = post.likes.includes(userId)  // checking if the user already like the post or not
+
+        if (userLikedPost) {
+            // unlike the post if already liked
+            await Post.updateOne({ _id: postId }, { $pull: { likes: userId }})
+            await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId }})
+            return res.status(200).json({ message: "Post Unliked Successfully" })
+        } else {
+            // like the post if already unliked
+            post.likes.push( userId )
+            await User.updateOne({ _id: userId }, { $push: { likedPosts: postId }})
+            await post.save();
+
+            const notification = new Notification({
+                from: userId,
+                to: post.user,
+                type: "like"
+            })
+
+            await notification.save();
+
+            return res.status(200).json({ message: "Post liked Successfully" })
+        }
+
     } catch (error) {
         console.log("Error in likeUnlikePost Controller: ", error.message)
         res.status(500).json({error: 'Internal Server Error'})
@@ -50,7 +137,28 @@ export const likeUnlikePost = async (req, res) => {
 
 export const commentOnPost = async (req, res) => {
     try {
-        
+        const { text } = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+
+        if (!text) {
+            return res.status(400).json({ error: "Text feild is required" })
+        }
+
+        const post = await Post.findById(postId)
+
+        if (!post) {
+            return res.status(404).json({ error: "Post not found" })
+        }
+
+        const comment = { user: userId, text}
+
+        post.comments.push(comment);
+
+        await post.save();
+
+        res.status(200).json(post)
+
     } catch (error) {
         console.log("Error in commentOnPost Controller: ", error.message)
         res.status(500).json({error: 'Internal Server Error'})
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 
 import { protectRoute } from "../middleware/protectRoute.js"
-import { commentOnPost, createPost, deletePost, likeUnlikePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost, getAllPosts, getLikedPosts, likeUnlikePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
+router.get("/all", protectRoute, getAllPosts)
+router.get("/liked/:id", protectRoute, getLikedPosts)
 router.post('/create', protectRoute, createPost)
 router.post('/like/:id', protectRoute, likeUnlikePost)
 router.post('/comment/:id', protectRoute, commentOnPost)
