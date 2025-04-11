29f10c9483c29af57e4f17bbcc4472de4525fa64
Prince Kanaugia <princekanaugia@gmail.com>
2025-04-11 16:45:07 +0530

    initial commit for frontend setup with tailwindcss and daisyui config setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
+
+import daisyUIThemes from "daisyui/src/theming/themes";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000
+  },
+  daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(29, 155, 240)",
+					secondary: "rgb(24, 24, 24)",
+				},
+			},
+		],
+	},
 })
